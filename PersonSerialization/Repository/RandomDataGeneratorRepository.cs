ea4800b834da7561929d8bf9aebacafa6328d9b8
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 17:33:47 +0300

    feat(RandomDataGeneratorRepository.cs): create random string-digits generator

--- PersonSerialization/Repository/RandomDataGeneratorRepository.cs
+++ PersonSerialization/Repository/RandomDataGeneratorRepository.cs
@@ -5,5 +5,15 @@
 /// </summary>
 public class RandomDataGeneratorRepository
 {
-	
+	/// <summary>
+	/// Generate random digits string with specific length
+	/// </summary>
+	/// <param name="length"></param>
+	/// <returns></returns>
+	private static string GenerateRandomDigits(int length)
+	{
+		Random random = new Random();
+		const string chars = "0123456789";
+		return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+	}
 }
