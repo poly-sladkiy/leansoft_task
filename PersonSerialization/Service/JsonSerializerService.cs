8fff60232b67c025d6c413c1c49615d40bf496ec
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 17:55:10 +0300

    feat(JsonSerializerService.cs): create service for json serialization/deserialization

---
+++ PersonSerialization/Service/JsonSerializerService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using PersonSerialization.Models;
+
+namespace PersonSerialization.Service;
+
+/// <summary>
+/// Json serializer
+/// </summary>
+public class JsonSerializerService
+{
+	private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+	{
+		DateFormatString = "yyyy-MM-ddTHH:mm:ssZ",
+		ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+	};
+
+	/// <summary>
+	/// Serialize object
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	public static string SerializeToJson(object? value)
+	{
+		return JsonConvert.SerializeObject(value, Settings);
+	}
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="json"></param>
+	/// <returns></returns>
+	public static T? DeserializeFromJson<T>(string json)
+	{
+		return JsonConvert.DeserializeObject<T>(json, Settings);
+	}
+}
