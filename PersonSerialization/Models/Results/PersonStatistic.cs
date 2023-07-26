8e842b0986879cc30d5ceb7face8f150acc3ba7c
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 18:03:05 +0300

    feat(PersonStatistic): record for collect statistic data

---
+++ PersonSerialization/Models/Results/PersonStatistic.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonSerialization.Models.Results;
+
+/// <summary>
+/// Persons statistic
+/// </summary>
+/// <param name="personCount"></param>
+/// <param name="creditCountCount"></param>
+/// <param name="avarageChildAge"></param>
+public record PersonStatistic(
+	int personCount,
+	int creditCountCount,
+	double avarageChildAge
+);
