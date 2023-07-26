866b93a571dd8e1edca7dbf79b6d17e08f31ed33
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 17:35:10 +0300

    feat(RandomDataGeneratorRepository.cs): create random credit card generator

--- PersonSerialization/Repository/RandomDataGeneratorRepository.cs
+++ PersonSerialization/Repository/RandomDataGeneratorRepository.cs
@@ -6,13 +6,23 @@
 public class RandomDataGeneratorRepository
 {
 	/// <summary>
+	/// Generate random credit card number
+	/// </summary>
+	/// <returns></returns>
+	public static string GenerateRandomCreditCard()
+	{
+		var cardNumber = "****-****-****-" + GenerateRandomDigits(4);
+		return cardNumber;
+	}
+	
+	/// <summary>
 	/// Generate random digits string with specific length
 	/// </summary>
 	/// <param name="length"></param>
 	/// <returns></returns>
 	private static string GenerateRandomDigits(int length)
 	{
-		Random random = new Random();
+		var random = new Random();
 		const string chars = "0123456789";
 		return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
 	}
