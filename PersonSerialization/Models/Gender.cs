c902da03141da005eb9a46f9c3c0c9619b7a3073
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 16:59:18 +0300

    style(Gender.cs): add docs comments

--- PersonSerialization/Models/Gender.cs
+++ PersonSerialization/Models/Gender.cs
@@ -1,7 +1,17 @@
 ﻿namespace PersonSerialization.Models;
 
+/// <summary>
+/// Gender
+/// </summary>
 enum Gender
 {
+	/// <summary>
+	/// Male
+	/// </summary>
 	Male,
+	
+	/// <summary>
+	/// Female
+	/// </summary>
 	Female
 }
