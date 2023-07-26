64d53e9bd1eec3e2688295806867e35a5483d8f3
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 18:22:12 +0300

    feat(Program.cs): done main logic

--- PersonSerialization/Program.cs
+++ PersonSerialization/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using PersonSerialization.Models;
+using PersonSerialization.Repository;
+using PersonSerialization.Service;
 
-Console.WriteLine("Hello, World!");
+const int personCount = 10000;
+const string fileName = "Persons.json";
+
+var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+var desktopFilePath = Path.Combine(desktopPath, fileName);
+
+var persons = PersonRepository.GenerateRandomPersons(personCount);
+var personJson = JsonSerializerService.SerializeToJson(persons);
+
+FileService.WriteToFile(desktopFilePath, personJson);
+persons.Clear();
+
+var personsFromFile = FileService.ReadFromFile(desktopFilePath);
+var personDeserializationFromFile = JsonSerializerService.DeserializeFromJson<List<Person>>(personsFromFile)
+	?? throw new NullReferenceException("Error person deserialization");
+
+var personStatistic = PersonRepository.CalculatePersonStatistic(personDeserializationFromFile);
+
+Console.WriteLine($"Persons Count: {personStatistic.personsCount}");
+Console.WriteLine($"Credit Card Count: {personStatistic.creditCardCount}");
+Console.WriteLine($"Average Child Age: {personStatistic.averageChildAge:F2} years");
+
+Console.ReadKey();
