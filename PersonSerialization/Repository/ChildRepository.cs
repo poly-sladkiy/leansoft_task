c73fa65c1e3cca16c159f87080ec819ebb2d0458
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 17:39:22 +0300

    feat(ChildRepository.cs): create class with GenerateRandomChild static method

---
+++ PersonSerialization/Repository/ChildRepository.cs
@@ -0,0 +1,21 @@
+﻿using PersonSerialization.Models;
+
+namespace PersonSerialization.Repository;
+
+public static class ChildRepository
+{
+	public static Child GenerateRandomChild()
+	{
+		var random = new Random();
+		var child = new Child
+		{
+			Id = random.Next(1000),
+			FirstName = "ChildFirstName_" + random.Next(10),
+			LastName = "ChildLastName_" + random.Next(10),
+			BirthDate = DateTimeOffset.FromUnixTimeSeconds(random.Next(0, (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds())).ToUnixTimeSeconds(),
+			Gender = random.Next(2) == 0 ? Gender.Male : Gender.Female
+		};
+
+		return child;
+	}
+}
