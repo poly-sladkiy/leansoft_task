745093694ff1ea19f10ad1aa61ee5a1a13774144
Konstantin Ignakov <ignakov37@gmail.com>
2023-07-26 16:58:05 +0300

    style(Child.cs): add docs comments

--- PersonSerialization/Models/Child.cs
+++ PersonSerialization/Models/Child.cs
@@ -1,10 +1,32 @@
 ﻿namespace PersonSerialization.Models;
 
+/// <summary>
+/// Child class
+/// </summary>
 class Child
 {
+	/// <summary>
+	/// Id
+	/// </summary>
 	public Int32 Id { get; set; }
+	
+	/// <summary>
+	/// Firstname
+	/// </summary>
 	public String FirstName { get; set; }
+	
+	/// <summary>
+	/// Lastname
+	/// </summary>
 	public String LastName { get; set; }
+	
+	/// <summary>
+	/// Birth date
+	/// </summary>
 	public Int64 BirthDate { get; set; }
+	
+	/// <summary>
+	/// Gender
+	/// </summary>
 	public Gender Gender { get; set; }
 }
